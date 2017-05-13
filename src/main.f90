f4d12b7e9e5abfc390a71eeb3b2540dbdd71c80c
Jatinder Pal Singh Sandhu <jayten010@gmail.com>
2017-05-13 22:25:35 +0530

    REMOVE: unused ierr variable

--- src/main.f90
+++ src/main.f90
@@ -12,23 +12,17 @@ program main
   include "mpif.h"
 #endif
 
-  integer :: ierr
-!  call MPI_INIT(ierr)
-!
-!  call setup_solver()
   call start_run()
 
 !  do while (.not. converged())
   do while (.true.)
-     !print *, 'Iteration', iter
-!     call flush()
      call step()
      if (current_iter == max_iters) then
         exit
      end if
      if (want_to_stop==1) max_iters=current_iter+1
   end do
+
   call finish_run()
-!  call destroy_solver()
-!  call MPI_FINALIZE(ierr)
+
 end program main
